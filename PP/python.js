26e54436f9d67101b7c60cc97496375a689ff006
lsascol01 <lsascol01@gmail.com>
2021-08-07 14:14:41 -0400

    ll

---
+++ PP/python.js
@@ -0,0 +1,6 @@
+lluvia = true
+sombrilla = true
+bajo_techo = true
+
+if(lluvia){if(sombrilla^bajo_techo){alert("Estás haciendo lo correcto")}else{alert("Estás haciendo lo incorrecto"}}& & alert("No necesitas sombrilla")
+ 
