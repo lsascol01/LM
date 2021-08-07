5cc4abf45008dba2883120e9fbb4b3fc819e8f19
lsascol01 <lsascol01@gmail.com>
2021-08-07 14:26:36 -0400

    m

--- PP/python.js
+++ PP/python.js
@@ -2,5 +2,44 @@ lluvia = true
 sombrilla = true
 bajo_techo = true
 
+{
+    if(sombrilla ^ bajo_techo)
+    {
+        alert("Estás haciendo lo correcto")
+    }
+    else
+    {
+        alert("Estás haciendo lo incorrecto")
+    }
+}
+else
+{
+    alert("No necesitas sombrilla")
+}
+
 if(lluvia){if(sombrilla^bajo_techo){alert("Estás haciendo lo correcto")}else{alert("Estás haciendo lo incorrecto"}}& & alert("No necesitas sombrilla")
- 
+
+if(lluvia)
+{
+
+
+Banco Cliente 
+Cuenta Cliente 
+Saldo Cliente 
+Banco destino 
+Cuenta destina 
+Hora de transferencia ]0-24]
+
+Transferir 1M USD
+
+Condiciones obligatorias de transferencia:
+Cliente verificado (verdadero o falso)
+Destino verificado (verdadero o falso)
+saldo mayor al monto a transferir mas costo de transacción
+Parametros del costo de transacción:
+Si el Banco destino es el mismo al Banco del Cliente el costo de transacción es 0 USD
+Si el Banco destino es diferente al Banco del Cliente el costo de transacción es 100 USD
+
+Solo se pueden hacer transferencias en hora de 9 a 12 o de 15 a 20
+
+Intenta hacer todo organizado por multiples lineas de codigo y también en un solo if
